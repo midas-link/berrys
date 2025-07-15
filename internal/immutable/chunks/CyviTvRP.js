adf0afcc28d4a3e96f24d8ca473a679c0fe6590e
Alireza Modiriyan <alireza.modiriyan98@gmail.com>
2025-07-15 16:19:12 +0100

    Updates

--- internal/immutable/chunks/CyviTvRP.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_bmt6pm)==null?void 0:s.base)??"/berrys";var t;const a=((t=globalThis.__sveltekit_bmt6pm)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ internal/immutable/entry/start.BP3xTjv6.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DqegutXa.js";export{o as load_css,r as start};
