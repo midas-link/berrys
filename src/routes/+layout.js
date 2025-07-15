50ba6b076eb7bdbeeda1566de3bf0d8e0870020d
Alireza Modiriyan <alireza.modiriyan98@gmail.com>
2025-07-15 12:13:02 +0100

    deploy fix

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -3,25 +3,26 @@
 
 import { goto } from '$app/navigation';
 import { browser } from '$app/environment';
+import { base } from '$app/paths';
+
 export const prerender = false;
 /** @type {import('./$types').LayoutLoad} */
-export async function load({url, data }) {
-  const dataFromParent = data; 
+export async function load({ url, data }) {
   if (browser) {
     const storedPath = sessionStorage.getItem('spaPath');
     if (storedPath) {
       sessionStorage.removeItem('spaPath');
-      const basePath = url.base;
-      const cleanPath = storedPath.startsWith(basePath) 
+      const basePath = base;
+      const cleanPath = storedPath.startsWith(basePath)
         ? storedPath.substring(basePath.length) || '/'
         : storedPath;
       if (url.pathname === basePath + '/' || url.pathname === '/') {
-        console.log(cleanPath);
+        // console.log(cleanPath);
         goto(cleanPath);
       }
     }
   }
   return {
-    user: dataFromParent && dataFromParent.user ? dataFromParent.user : null
+    user: data && data.user ? data.user : null
   };
 }
